fb89d31d274d6ca47c86ffc280ca11b8594a8c50
KIMCHAN\KCLAB <ckim.esw@gmail.com>
2022-03-13 18:36:55 +0900

    CHAPTER2 실습 수업

---
+++ CHAPTER2/chapter2/chapter2/homework1.c
@@ -0,0 +1,9 @@
+// 화면에 두 줄을 출력한다.
+
+#include <stdio.h>
+
+int main() {
+	printf("Hello World!\n");
+	printf("Kim Chan");
+	return 0;
+}
---
+++ CHAPTER2/chapter2/chapter2/homework2.c
@@ -0,0 +1,10 @@
+// 화면에 여러 줄을 출력한다.
+
+#include <stdio.h>
+
+int main() {
+	printf("3x1=3\n");
+	printf("3x2=6\n");
+	printf("3x3=9\n");
+	return 0;
+}
