e0bde3a1b278b2b895ad0bc82ce9b3dc6c3ad529
KIMCHAN\KCLAB <ckim.esw@gmail.com>
2022-03-15 14:46:21 +0900

    Update: CHAPTER2 Readme.md

---
+++ CHAPTER2/chapter2/chapter2/homework5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	int a = 123456;
+	float b = 123.456;
+	double c = 123.456;
+	long double d = 123.456;
+
+	printf("a: %d\n", a);
+	printf("b: %f\n", b);
+	printf("c: %f\n", c);
+	printf("d: %lf\n", d);
+
+	return 0;
+}
